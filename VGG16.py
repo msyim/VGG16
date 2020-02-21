5727d60537c6e7a6d32c67a45f068ac1f5fa12f5
Minsub Yim <msyim826@gmail.com>
2020-02-21 05:59:38 +0000

    changed tabspace to 4 and added scheduler

--- VGG16.py
+++ VGG16.py
@@ -4,7 +4,7 @@ import torchvision.datasets as dsets
 import torchvision.transforms as transforms
 
 BATCH_SIZE = 10
-LEARNING_RATE = 0.001
+LEARNING_RATE = 0.01
 EPOCH = 50
 N_CLASSES = 25
 
@@ -45,66 +45,64 @@ def vgg_fc_layer(size_in, size_out):
     return layer
 
 class VGG16(tnn.Module):
-  def __init__(self, n_classes=1000):
-    super(VGG16, self).__init__()
+    def __init__(self, n_classes=1000):
+        super(VGG16, self).__init__()
 
-    # Conv blocks (BatchNorm + ReLU activation added in each block)
-    self.layer1 = vgg_conv_block([3,64], [64,64], [3,3], [1,1], 2, 2)
-    self.layer2 = vgg_conv_block([64,128], [128,128], [3,3], [1,1], 2, 2)
-    self.layer3 = vgg_conv_block([128,256,256], [256,256,256], [3,3,3], [1,1,1], 2, 2)
-    self.layer4 = vgg_conv_block([256,512,512], [512,512,512], [3,3,3], [1,1,1], 2, 2)
-    self.layer5 = vgg_conv_block([512,512,512], [512,512,512], [3,3,3], [1,1,1], 2, 2)
+        # Conv blocks (BatchNorm + ReLU activation added in each block)
+        self.layer1 = vgg_conv_block([3,64], [64,64], [3,3], [1,1], 2, 2)
+        self.layer2 = vgg_conv_block([64,128], [128,128], [3,3], [1,1], 2, 2)
+        self.layer3 = vgg_conv_block([128,256,256], [256,256,256], [3,3,3], [1,1,1], 2, 2)
+        self.layer4 = vgg_conv_block([256,512,512], [512,512,512], [3,3,3], [1,1,1], 2, 2)
+        self.layer5 = vgg_conv_block([512,512,512], [512,512,512], [3,3,3], [1,1,1], 2, 2)
 
-    # FC layers
-    self.layer6 = vgg_fc_layer(7*7*512, 4096)
-    self.layer7 = vgg_fc_layer(4096, 4096)
+        # FC layers
+        self.layer6 = vgg_fc_layer(7*7*512, 4096)
+        self.layer7 = vgg_fc_layer(4096, 4096)
 
-    # Final layer
-    self.layer8 = tnn.Sequential(
-        # 8 output layer
-        tnn.Linear(4096, n_classes)
-    )
+        # Final layer
+        self.layer8 = tnn.Linear(4096, n_classes)
 
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        vgg16_features = self.layer5(out)
+        out = vgg16_features.view(out.size(0), -1)
+        out = self.layer6(out)
+        out = self.layer7(out)
+        out = self.layer8(out)
 
-  def forward(self, x):
-      out = self.layer1(x)
-      out = self.layer2(out)
-      out = self.layer3(out)
-      out = self.layer4(out)
-      vgg16_features = self.layer5(out)
-      out = vgg16_features.view(out.size(0), -1)
-      out = self.layer6(out)
-      out = self.layer7(out)
-      out = self.layer8(out)
-
-      return vgg16_features, out
+        return vgg16_features, out
 
       
 vgg16 = VGG16(n_classes=N_CLASSES)
 vgg16.cuda()
 
-# Loss and Optimizer
+# Loss, Optimizer & Scheduler
 cost = tnn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(vgg16.parameters(), lr=LEARNING_RATE)
+scheduler = torch.optim.scheduler.ReduceLROnPlateau(optimizer)
 
 # Train the model
 for epoch in range(EPOCH):
 
-  avg_loss = 0
-  cnt = 0
-  for images, labels in trainLoader:
-    images = images.cuda()
-    labels = labels.cuda()
+    avg_loss = 0
+    cnt = 0
+    for images, labels in trainLoader:
+        images = images.cuda()
+        labels = labels.cuda()
 
-    # Forward + Backward + Optimize
-    optimizer.zero_grad()
-    _, outputs = vgg16(images)
-    loss = cost(outputs, labels)
-    avg_loss += loss.data
-    cnt += 1
-    print("[E: %d] loss: %f, avg_loss: %f" % (epoch, loss.data, avg_loss/cnt))
-    loss.backward()
-    optimizer.step()
+        # Forward + Backward + Optimize
+        optimizer.zero_grad()
+        _, outputs = vgg16(images)
+        loss = cost(outputs, labels)
+        avg_loss += loss.data
+        cnt += 1
+        print("[E: %d] loss: %f, avg_loss: %f" % (epoch, loss.data, avg_loss/cnt))
+        loss.backward()
+        optimizer.step()
+    scheduler.step(avg_loss)
 
 # Test the model
 vgg16.eval()
@@ -112,13 +110,13 @@ correct = 0
 total = 0
 
 for images, labels in testLoader:
-  images = images.cuda()
-  _, outputs = vgg16(images)
-  _, predicted = torch.max(outputs.data, 1)
-  total += labels.size(0)
-  correct += (predicted.cpu() == labels).sum()
-  print(predicted, labels, correct, total)
-  print("avg acc: %f" % (100* correct/total))
+    images = images.cuda()
+    _, outputs = vgg16(images)
+    _, predicted = torch.max(outputs.data, 1)
+    total += labels.size(0)
+    correct += (predicted.cpu() == labels).sum()
+    print(predicted, labels, correct, total)
+    print("avg acc: %f" % (100* correct/total))
 
 # Save the Trained Model
 torch.save(vgg16.state_dict(), 'cnn.pkl')
